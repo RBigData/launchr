3a766ad662d33887536c5b278721b19eb1792638
George Ostrouchov <ostrouchovg@utk.edu>
2018-08-29 11:28:17 -0400

    del

--- example/start_rhea_server.r
+++
@@ -1,5 +0,0 @@
-library(launchr)
-
-launch(nodes = 1, npernode = 16, server = "rhea.ccs.ornl.gov",
-       modules = c("r"), user = "ost", account = "gen001",
-       walltime = "01:00:00", rwd = "~/demo")
