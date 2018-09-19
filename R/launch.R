c5314971ceed3b03624fc4502c99a306c57fe5a9
George Ostrouchov <ostrouchovg@utk.edu>
2018-09-19 00:04:16 -0400

    partial revert after rebase

--- R/launch.R
+++ R/launch.R
@@ -39,66 +39,6 @@ make_pbs_script = function(nodes, npernode, rwd, modules, walltime, server,
     script
 }
 
-## #' Launches a persistent pbdR server on a cluster
-## #'
-## #' @param nodes The number of nodes to allocate for the server.
-## #' @param npernode The number of MPI ranks (R sessions) to start per node.
-## #' @param walltime A string "hh:mm:ss" giving the maximum length of time
-## #'                 in hours:minutes:seconds before server timeout shutdown.
-## #' @param user Username for login.
-## #' @param server Hostname to run the pbdR server, resolvable on your
-## #'  platform.
-## #' @param account Account used for your cluster allocation
-## #' @param modules A vector of strings giving the modules to load before
-## #' starting the server.
-## #' @param rwd Remote working directory as a string.
-## #' @return A shell script as a vector of strings.
-## #' @export
-## launch = function(nodes = 1, npernode = 16, server = "set_me",
-##                   modules = NULL, user = Sys.getenv("USER"),
-##                   account = NULL, walltime = "01:00:00", rwd = "~/") {
-##     ## parameters that will eventually migrate to control structures
-##     if(is.null(modules))
-##         return(print("Please specify at least one module"))
-##     if(is.null(account))
-##         return(print("Please specify account"))
-
-##     port = 55555
-
-##     if(server == "rhea.ccs.ornl.gov") {
-##         pscript = preload_rhea(nodes, npernode, account = account, rwd = rwd,
-##                                modules = modules, port = port)
-
-##         args = args_rhea(port)
-
-##         rserver = pbdRPC::machine(hostname = server,
-##                                   user = user,
-##                                   exec.type = "ssh",
-##                                   args = args)
-
-##     } else if(server == "or-condo-login.ornl.gov") {
-##         pscript = preload_or_condo(nodes, npernode, account = account,
-##                                    rwd = rwd,
-##                                    modules = modules, port = port)
-
-##         args = args_or_condo(port)
-
-##         rserver = pbdRPC::machine(hostname = server,
-##                                   user = user,
-##                                   exec.type = "ssh",
-##                                   args = args)
-
-##     } else {
-##         cat("Don't know how to launch on", server, "yet\n")
-##         return(invisible(FALSE))
-##     }
-
-##     pbdRPC::start_cs(machine = rserver, cmd = "",
-##                      preload = paste0(pscript, collapse = "\n"))
-##     return(invisible(TRUE))
-
-## }
-
 #' Launches a persistent pbdR server on a cluster
 #'
 #' @param nodes The number of nodes to allocate for the server.
@@ -114,38 +54,47 @@ make_pbs_script = function(nodes, npernode, rwd, modules, walltime, server,
 #' @param rwd Remote working directory as a string.
 #' @return A shell script as a vector of strings.
 #' @export
-launch = function(nodes, npernode = 16, server = NULL,
+launch = function(nodes = 1, npernode = 16, server = "set_me",
                   modules = NULL, user = Sys.getenv("USER"),
-                  account = NULL, walltime = "01:00:00", rwd = "~/",
-                  port = 55555) {
+                  account = NULL, walltime = "01:00:00", rwd = "~/") {
     ## parameters that will eventually migrate to control structures
     if(is.null(modules))
         return(print("Please specify at least one module"))
-    if(is.null(server))
-        return(print("Server not specified"))
     if(is.null(account))
         return(print("Please specify account"))
 
-    ## set file names to be created in rwd directory.
-    fn = list(pbs_file = ".pbdR_server.pbs",
-              head_node_file = ".pbdR_hostname.txt",
-              lnode_file = ".pbdR_lnode.sh"
-              )
+    port = 55555
 
-    pscript = ssh_tunnel_qsub(nodes, npernode, account = account,
-                              rwd = rwd, pbs_FUN = make_pbs_script,
-                              modules = modules, port = port, fn = fn)
-    args = args_tunnel(port)
+    if(server == "rhea.ccs.ornl.gov") {
+        pscript = preload_rhea(nodes, npernode, account = account, rwd = rwd,
+                               modules = modules, port = port)
 
-    rserver = pbdRPC::machine(hostname = server,
-                              user = user,
-                              exec.type = "ssh",
-                              args = args)
+        args = args_rhea(port)
+
+        rserver = pbdRPC::machine(hostname = server,
+                                  user = user,
+                                  exec.type = "ssh",
+                                  args = args)
+
+    } else if(server == "or-condo-login.ornl.gov") {
+        pscript = preload_or_condo(nodes, npernode, account = account,
+                                   rwd = rwd,
+                                   modules = modules, port = port)
+
+        args = args_or_condo(port)
+
+        rserver = pbdRPC::machine(hostname = server,
+                                  user = user,
+                                  exec.type = "ssh",
+                                  args = args)
+
+    } else {
+        cat("Don't know how to launch on", server, "yet\n")
+        return(invisible(FALSE))
+    }
 
     pbdRPC::start_cs(machine = rserver, cmd = "",
                      preload = paste0(pscript, collapse = "\n"))
-
     return(invisible(TRUE))
 
 }
-
