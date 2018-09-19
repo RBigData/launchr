25a8a143d981de6f1cf5bc07f6d15a900424a363
George Ostrouchov <ostrouchovg@utk.edu>
2018-09-19 14:54:22 -0400

    first commit of reorg

--- R/launch.R
+++ R/launch.R
@@ -1,100 +1,67 @@
-
-#' @export
-make_pbs_script = function(nodes, npernode, rwd, modules, walltime, server,
-                           account, warn_on_fork, fn) {
-    ## create qsub (head node) PBS script
-    pbs_common= c(
-        "#!/bin/bash",
-        "#PBS -N pbdR_server",
-        "#PBS -e .pbdR_server.e",
-        "#PBS -o .pbdR_server.o",
-        paste0("#PBS -A ", account),
-        paste0("#PBS -l walltime=", walltime)
-    )
-    pbs_unique =
-        switch(server,
-               "rhea.ccs.ornl.gov" = paste0("#PBS -l nodes=", nodes),
-               "or-condo-login.ornl.gov" = c(
-                   paste0("#PBS -l nodes=", nodes, ":ppn=32"),
-                   "#PBS -l qos=std",
-                   "#PBS -q batch",
-                   "#PBS -W group_list=cades-ccsd",
-                   "#PBS -m abe"
-               ),
-               stop(paste("Don't know how to connect to", machine, " yet!"))
-               )
-    mod_vec = vector("character", length(modules))
-    for(m in seq(along=modules))
-        mod_vec[m] = paste0("module load ", modules[m])
-    wof = ifelse(warn_on_fork, "", " --mca mpi_warn_on_fork 0 ")
-    commands = c(
-        "module list",
-        paste0("cd ", rwd),
-        paste0("hostname > ", fn$head_node_file),
-        paste0("mpirun ", wof, " --map-by ppr:", npernode,
-               ":node Rscript -e 'pbdCS::pbdserver()'")
-    )
-    script = c(pbs_common, pbs_unique, mod_vec, commands)
-
-    script
-}
-
 #' Launches a persistent pbdR server on a cluster
-#'
-#' @param nodes The number of nodes to allocate for the server.
-#' @param npernode The number of MPI ranks (R sessions) to start per node.
-#' @param walltime A string "hh:mm:ss" giving the maximum length of time
+#' @param SERVER A string giving the hostname to run the pbdR server, resolvable on your
+#'   local platform.
+#' @param user Username for server login.
+#' @param FUN The function that returns a PBS script and a login node script
+#' @param verbose If TRUE, print generated scripts before submission. A
+#' value > 1 will also prevent script submission.
+#' @param ... Arguments to be passed to FUN for script details. Typical
+#' parameters include
+#' \describe{
+#'   \item{nodes} The number of nodes to allocate for the server.
+#'   \item{npernode} The number of MPI ranks (R sessions) to start per node.
+#'   \item{walltime} A string "hh:mm:ss" giving the maximum length of time
 #'                 in hours:minutes:seconds before server timeout shutdown.
-#' @param user Username for login.
-#' @param server Hostname to run the pbdR server, resolvable on your
-#'  platform.
-#' @param account Account used for your cluster allocation
-#' @param modules A vector of strings giving the modules to load before
-#' starting the server.
-#' @param rwd Remote working directory as a string.
+#'   \item{account} Account used for your cluster allocation
+#'   \item{modules} A vector of strings giving the modules to load before
+#'     starting the server.
+#'   \item{rwd} Remote working directory as a string.
+#' }
 #' @return A shell script as a vector of strings.
 #' @export
-launch = function(nodes = 1, npernode = 16, server = "set_me",
-                  modules = NULL, user = Sys.getenv("USER"),
-                  account = NULL, walltime = "01:00:00", rwd = "~/") {
-    ## parameters that will eventually migrate to control structures
-    if(is.null(modules))
-        return(print("Please specify at least one module"))
-    if(is.null(account))
-        return(print("Please specify account"))
+launch = function(SERVER, user = Sys.getenv("USER"), FUN = NULL,
+                  verbose = FALSE, ...) {
+    ## generate default script
+    preload = lnode_script(...)
+    args = ssh_args()
+    ## TODO try to infer script form server name
+    ## server = strsplit(SERVER, "[.]")[1]
+
+    if(verbose) {
+        print("Script for login node:")
+        print(preload)
+        print("Local ssh parameters:")
+        print(args)
+
+        if(verbose > 1) return("Printing script only. No launch")
+    }
+
+    ## now process any modifications to the default by the FUN filter
+    if(!is.null(FUN)) {
+        ## does the function exist in user's environment?
+        FUN_mod = try(match.fun(FUN, silent=TRUE))
+        if(class(FUN_mod) == "try-error") {
+            ## next, try to find it in inst/templates of launchr
+            template = system.file(server_fun, "templates", package = "launchr")
+            if(length(template) > 0) {
+                FUN_mod = template
+            } else {
+                return("FUN function not found")
+            }
+        }
+
+    }
+### I am here ... now reprocess default scripts with FUN or FUN_mod
 
     port = 55555
 
-    if(server == "rhea.ccs.ornl.gov") {
-        pscript = preload_rhea(nodes, npernode, account = account, rwd = rwd,
-                               modules = modules, port = port)
+    scripts = FUN(...)
 
-        args = args_rhea(port)
-
-        rserver = pbdRPC::machine(hostname = server,
-                                  user = user,
-                                  exec.type = "ssh",
-                                  args = args)
-
-    } else if(server == "or-condo-login.ornl.gov") {
-        pscript = preload_or_condo(nodes, npernode, account = account,
-                                   rwd = rwd,
-                                   modules = modules, port = port)
-
-        args = args_or_condo(port)
-
-        rserver = pbdRPC::machine(hostname = server,
-                                  user = user,
-                                  exec.type = "ssh",
-                                  args = args)
-
-    } else {
-        cat("Don't know how to launch on", server, "yet\n")
-        return(invisible(FALSE))
-    }
+    rserver = pbdRPC::machine(hostname = SERVER, user = user, exec.type = "ssh",
+                              args = scripts$args)
 
     pbdRPC::start_cs(machine = rserver, cmd = "",
-                     preload = paste0(pscript, collapse = "\n"))
+                     preload = paste0(scripts$preload, collapse = "\n"))
     return(invisible(TRUE))
 
 }
--- R/preload.r
+++ R/preload.r
@@ -12,44 +12,76 @@ file_it = function(script, file_name, eof = NULL) {
     c(paste0("cat >> ", file_name, " << ", "'", eof, "'"), script, eof)
 }
 
+#' Creates a default pbs script as a vector of strings
+#' @param nodes Number of nodes to use for the server
+#' @param npernode Number of R instances to run per node
+#' @param rwd Working directory on server. This is where script files
+#'     are created.
+#' @param modules A vector of strings giving the modules to load
+#'     before starting the server.
+#' @param walltime A string "hh:mm:ss" giving the maximum length of
+#'     time in hours:minutes:seconds before server timeout
+#'     shutdown. Note that while the server is running you may be
+#'     incurring time charges to your account.
+#' @param account Account used for your cluster allocation
+#' @param warn_on_fork Set to \code{FALSE} to suppress a warning when
+#'     forking a process (for example, when invoking
+#'     \code{parallel::mclapply()}).
+#' @param fn A list providing names used for script files that are
+#'     created in the server rwd directory.
+#' @return A shell script as a vector of strings.
+pbs_default = function(nodes = 1, npernode = 16, rwd = "~/", modules = "r",
+                       walltime = "01:00:00",
+                       account, warn_on_fork = TRUE, fn, ...) {
+  pbs = c(
+    "#!/bin/bash",
+    "#PBS -N pbdR_server",
+    "#PBS -j oe",
+    "#PBS -o .pbdR_server.o",
+    paste0("#PBS -A ", account),
+    paste0("#PBS -l walltime=", walltime)
+    paste0("#PBS -l nodes=", nodes)
+  )
+
+  mod_vec = vector("character", length(modules))
+  for(m in seq(along=modules))
+    mod_vec[m] = paste0("module load ", modules[m])
+
+  wof = ifelse(warn_on_fork, "", " --mca mpi_warn_on_fork 0 ")
+
+  commands = c(
+    "module list",
+    paste0("cd ", rwd),
+    paste0("hostname > ", fn$head_node_file),
+    paste0("mpirun ", wof, " --map-by ppr:", npernode,
+           ":node Rscript -e 'pbdCS::pbdserver()'")
+  )
+
+  c(pbs, mod_vec, commands)
+}
+
 #' Constructs a shell script as a vector of strings. Its purpose is to run
-#' on a login node of rhea.ccs.ornl.gov, submit a pbdR server batch job that
+#' in a login node shell and submit a pbdR server batch job that
 #' runs for a specified time or until closed. It also opens an ssh tunnel
 #' from the login node to the running server head node.
 #'
-#' @param nodes The number of nodes to allocate for the server.
-#' @param npernode The number of MPI ranks (R sessions) to start per node.
-#' @param walltime A string "hh:mm:ss" giving the maximum length of time
-#'                 in hours:minutes:seconds before server timeout shutdown.
-#' @param user Username for login.
-#' @param machine Hostname to run the server, resolvable on your platform.
 #' @param port The port used by the server.
-#' @param account Account used for your cluster allocation
-#' @param modules A vector of strings giving the modules to load before
-#' starting the server.
 #' @param rwd Remote working directory as a string.
 #' @param verbose Should the preload command be printed?
-#' @param warn_on_fork Set to \code{FALSE} to suppress a warning when forking
-#' a process (for example, when invoking \code{parallel::mclapply()}).
 #' @return A shell script as a vector of strings.
-ssh_tunnel_qsub = function(nodes = 1, npernode=16, walltime = "01:00:00",
-                          user = NULL, machine = NULL, pbs_FUN = NULL,
-                          account = NULL, modules=c("r"), rwd = "~",
-                          show = FALSE,  port = 55555, warn_on_fork = TRUE,
-                          fn) {
+lnode_script = function(port = 55555, ...) {
     ## Returns a script as a vector of strings to run on a login node so that it
     ##    starts a pbdR server and opens an ssh tunnel to its head node on
     ##    the specified port
 
     ## set working directory and file names to be created there
-    cd = paste0("cd ", rwd)
+    cd = paste0("cd ", rwd) # TODO where will this be matched?
 
     ## file cleanup command
     clean = paste("rm -f", fn$pbs_file, fn$head_node_file, fn$lnode_file)
 
     ## commands to write pbs script to its file
-    pbs_script = pbs_FUN(nodes, npernode, rwd, modules, walltime, server,
-                         account, warn_on_fork=TRUE, fn)
+    pbs_script = pbs_default(...)
     make_pbs_file = file_it(pbs_script, fn$pbs_file, "..PBS-EOF..")
 
     ## command to qsub the script
@@ -77,211 +109,12 @@ ssh_tunnel_qsub = function(nodes = 1, npernode=16, walltime = "01:00:00",
     run_file = paste0("source ", fn$lnode_file)
 
     ## put it all together
-    lnode_command = c(cd, clean, make_lnode_file, run_file)
-    if(show) print(lnode_command)
-
-    lnode_command
+    c(cd, clean, make_lnode_file, run_file)
 }
 
-#' Constructs a string of arguments for ssh exection on Rhea
+#' Constructs a string of arguments for local ssh to forward a port
 #' @param port The port for server connection
-#' @param verbose Should the args be printed?
 #' @value A string of ssh arguments.
-args_tunnel = function(port = 55555, show = FALSE) {
+ssh_args = function(port = 55555) {
     args = paste0(" -f -L ", port, ":localhost:", port)
-    if(show) print(args)
-    args
-}
-
-#' Constructs a shell script as a vector of strings. Its purpose is to run
-#' on a login node of rhea.ccs.ornl.gov, submit a pbdR server batch job that
-#' runs for a specified time or until closed. It also opens an ssh tunnel
-#' from the login node to the running server head node.
-#'
-#' @param nodes The number of nodes to allocate for the server.
-#' @param npernode The number of MPI ranks (R sessions) to start per node.
-#' @param walltime A string "hh:mm:ss" giving the maximum length of time
-#'                 in hours:minutes:seconds before server timeout shutdown.
-#' @param user Username for login.
-#' @param machine Hostname to run the server, resolvable on your platform.
-#' @param port The port used by the server.
-#' @param account Account used for your cluster allocation
-#' @param modules A vector of strings giving the modules to load before
-#' starting the server.
-#' @param rwd Remote working directory as a string.
-#' @param verbose Should the preload command be printed?
-#' @param warn_on_fork Set to \code{FALSE} to suppress a warning when forking
-#' a process (for example, when invoking \code{parallel::mclapply()}).
-#' @return A shell script as a vector of strings.
-preload_rhea = function(nodes = 1, npernode=16, walltime = "01:00:00",
-                        user = NULL, machine = "rhea.ccs.ornl.gov",
-                        port = 55555,
-                        account = NULL, modules=c("r"), rwd = "~",
-                        show = FALSE, warn_on_fork = TRUE) {
-    ## Returns a script as a vector of strings to run on a login node so that it
-    ##    starts a pbdR server and opens an ssh tunnel to its head node on
-    ##    the specified port
-
-    ## set working directory and file names to be created there
-    cd = paste0("cd ", rwd)
-    pbs_file = ".pbdR_server.pbs" # script to run on head node
-    head_node_file = ".pbdR_hostname"
-    lnode_file = ".pbdR_lnode.sh"
-
-    ## file cleanup command
-    clean = paste("rm -f", pbs_file, head_node_file, lnode_file)
-
-    ## create qsub (head node) PBS script
-    wof = ifelse(warn_on_fork, "", " --mca mpi_warn_on_fork 0 ")
-    pbs_parameters = c(
-        "#!/bin/bash",
-        "# File name: pbdR_server",
-        paste0("#PBS -A ", account),
-        paste0("#PBS -l walltime=", walltime),
-        paste0("#PBS -l nodes=", nodes),
-        "#PBS -e pbdR_server.e",
-        "#PBS -o pbdR_server.o"
-    )
-    mod_vec = vector("character", length(modules))
-    for(m in seq(along=modules))
-        mod_vec[m] = paste0("module load ", modules[m])
-    commands = c(
-        "module list",
-        cd,
-        paste0("hostname > ", head_node_file),
-        paste0("mpirun ", wof, " --map-by ppr:", npernode,
-               ":node Rscript -e 'pbdCS::pbdserver()'")
-    )
-    pbs_script = c(pbs_parameters, mod_vec, commands)
-
-    ## commands to write head script to its file
-    make_pbs_file = file_it(pbs_script, pbs_file, "..PBS-EOF..")
-
-    ## command to qsub the script
-    qsub = paste0("qsub ", pbs_file)
-
-    ## commands to wait for server start with progress dots
-    wait_run = paste0("while [ ! -f ", head_node_file,
-                      " ]; do sleep 1; echo -n '.'; done; echo '.' ")
-
-    ## command to tunnel from login to head node
-    tunnel = paste0("ssh -f -L ", port, ":localhost:", port,
-                    " -N \\$(cat ", head_node_file, ")")
-
-    ## commands to report login node and head node names
-    tell = c("echo 'server login node: ' \\$(hostname)",
-             paste0("echo 'server head node: ' \\$(cat ", head_node_file, ")"))
-
-    lnode_script = c(cd, make_pbs_file, qsub, wait_run, tunnel, tell)
-
-    ## commands to write login node script to its file
-    make_lnode_file = file_it(lnode_script, lnode_file, "..LNODE-EOF..")
-
-    ## command to run the login node script
-    run_file = paste0("source ", lnode_file)
-
-    ## put it all together
-    lnode_command = c(cd, clean, make_lnode_file, run_file)
-    if(show) print(lnode_command)
-
-    lnode_command
-}
-
-#' Constructs a string of arguments for ssh exection on Rhea
-#' @param port The port for server connection
-#' @param verbose Should the args be printed?
-#' @value A string of ssh arguments.
-args_rhea = function(port = 55555, show = FALSE) {
-    args = paste0(" -f -L ", port, ":localhost:", port)
-    if(show) print(args)
-    args
-}
-
-preload_or_condo = function(nodes = 1, npernode=16, walltime = "01:00:00",
-                           user = NULL, machine = "rhea.ccs.ornl.gov",
-                           port = 55555,
-                           account = NULL, modules=c("R"), rwd = "~",
-                           show = FALSE, warn_on_fork = TRUE) {
-    ## Returns a script as a vector of strings to run on a login node so that it
-    ##    starts a pbdR server and opens an ssh tunnel to its head node on
-    ##    the specified port
-
-    ## set working directory and file names to be created there
-    cd = paste0("cd ", rwd)
-    pbs_file = ".pbdR_server.pbs" # script to run on head node
-    head_node_file = ".pbdR_hostname"
-    lnode_file = ".pbdR_lnode.sh"
-
-    ## file cleanup command
-    clean = paste("rm -f", pbs_file, head_node_file, lnode_file)
-
-    ## create qsub (head node) PBS script
-    wof = ifelse(warn_on_fork, "", " --mca mpi_warn_on_fork 0 ")
-    pbs_parameters = c(
-        "#!/bin/bash",
-        "#PBS -N pbdR_server",
-        "#PBS -e pbdR_server.e",
-        "#PBS -o pbdR_server.o",
-        paste0("#PBS -A ", account),
-        paste0("#PBS -l walltime=", walltime),
-        paste0("#PBS -l nodes=", nodes, ":ppn=32"),
-        "#PBS -l qos=std",
-        "#PBS -q batch",
-        "#PBS -W group_list=cades-ccsd",
-        "#PBS -m abe"
-    )
-    mod_vec = vector("character", length(modules))
-    for(m in seq(along=modules))
-        mod_vec[m] = paste0("module load ", modules[m])
-    commands = c(
-        "module list",
-        cd,
-        paste0("hostname > ", head_node_file),
-        paste0("mpirun ", wof, " --map-by ppr:", npernode,
-               ":node Rscript -e 'pbdCS::pbdserver()'")
-    )
-    pbs_script = c(pbs_parameters, mod_vec, commands)
-
-    ## commands to write head script to its file
-    make_pbs_file = file_it(pbs_script, pbs_file, "..PBS-EOF..")
-
-    ## command to qsub the script
-    qsub = paste0("qsub ", pbs_file)
-
-    ## commands to wait for server start with progress dots
-    wait_run = paste0("while [ ! -f ", head_node_file,
-                      " ]; do sleep 1; echo -n '.'; done; echo '.' ")
-
-    ## command to tunnel from login to head node
-    tunnel = paste0("ssh -f -L ", port, ":localhost:", port,
-                    " -N \\$(cat ", head_node_file, ")")
-
-    ## commands to report login node and head node names
-    tell = c("echo 'server login node: ' \\$(hostname)",
-             paste0("echo 'server head node: ' \\$(cat ", head_node_file, ")"))
-
-    lnode_script = c(cd, make_pbs_file, qsub, wait_run, tunnel, tell)
-
-    ## commands to write login node script to its file
-    make_lnode_file = file_it(lnode_script, lnode_file, "..LNODE-EOF..")
-
-    ## command to run the login node script
-    run_file = paste0("source ", lnode_file)
-
-    ## put it all together
-    lnode_command = c(cd, clean, make_lnode_file, run_file)
-    if(show) print(lnode_command)
-
-    lnode_command
-}
-
-#' Constructs a string of arguments for ssh exection on Rhea
-#' @param port The port for server connection
-#' @param verbose Should the args be printed?
-#' @value A string of ssh arguments.
-args_or_condo = function(port = 55555, show=FALSE) {
-    args = paste0(" -f -L ", port, ":localhost:", port)
-    if(show) print(args)
-
-    args
 }
